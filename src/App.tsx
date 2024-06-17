d5e68872669a7bfb20c664efcca38efff91dc22e
Leonard <leonard@mimisiku.dev>
2024-06-17 11:59:12 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [amount, setAmount] = useState(0);
+  const [paymentMode, setPaymentMode] = useState(0);
+  const [paymentConfirm, setPaymentConfirm] = useState(false);
+
+  return (
+    <>
+      <h1>Lyzi payment</h1>
+      <div className="card">
+        <p>
+          <span>Amount :</span>
+          <input type="number" onInput={(e) => {
+            setAmount(parseFloat((e.target as HTMLInputElement).value));
+          }}></input>
+          Amount: {amount}
+        </p>
+      </div>
+      <div>
+        <h3>Payment options :</h3>
+        <div>
+          <button onClick={() => setPaymentMode(0)}>Transfer</button>
+          <button onClick={() => setPaymentMode(1)}>Card</button>
+          <button onClick={() => setPaymentMode(2)}>Crypto</button>
+        </div>
+        {paymentMode === 0 ? <div>
+          <h4>Transfer</h4>
+          <div>Send to FR76 1000 1000 1000 1000</div>
+        </div> : <div></div>}
+        {paymentMode === 1 ? <div>
+          <h4>Card</h4>
+          <label>Card number </label>
+          <input type="text"></input>
+        </div> : <div></div>}
+        {paymentMode === 2 ? <div>
+          <h4>Crypto</h4>
+          <div>Send to wallet 1Lbcfr7sAHTD9CgdQo3HTMTkV8LK4ZnX71</div>
+          <button onClick={() => {
+            setTimeout(() => {
+              setPaymentConfirm(true);
+            }, 500);
+          }}>Confirm</button>
+          {paymentConfirm ? <div>Confirm received</div> : <div></div>}
+        </div> : <div></div>}
+      </div>
+    </>
+  )
+}
+
+export default App
