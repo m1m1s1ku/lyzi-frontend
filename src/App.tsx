1b4dccfa651682ff6ed396647148171e3731c1a3
Leonard <leonard@mimisiku.dev>
2024-06-17 12:30:34 +0200

    Add validation on amount

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,19 @@ function App() {
   const [amount, setAmount] = useState(0);
   const [paymentMode, setPaymentMode] = useState(0);
   const [paymentConfirm, setPaymentConfirm] = useState(false);
+  const [invalidAmount, setInvalidAmount] = useState(false);
+
+  const onConfirm = () => {
+    if(!amount || isNaN(amount)) {
+      setInvalidAmount(true);
+      setPaymentConfirm(false);
+      return;
+    }
+
+    setTimeout(() => {
+      setPaymentConfirm(true);
+    }, 1000);
+  }
 
   return (
     <>
@@ -13,8 +26,10 @@ function App() {
         <p>
           <span>Amount :</span>
           <input type="number" onInput={(e) => {
+            setInvalidAmount(false);
             setAmount(parseFloat((e.target as HTMLInputElement).value));
           }}></input>€
+          {invalidAmount ? <div>Invalid amount</div> : <div></div>}
         </p>
       </div>
       <div>
@@ -27,22 +42,22 @@ function App() {
         {paymentMode === 0 ? <div>
           <h4>Transfer</h4>
           <div>Send {amount}€ to FR76 1000 1000 1000 1000</div>
+          <button onClick={onConfirm}>Confirm</button>
+          {paymentConfirm ? <div>Confirm received</div> : <div></div>}
         </div> : <div></div>}
         {paymentMode === 1 ? <div>
           <h4>Card</h4>
           <p>Amount : {amount}€</p>
           <label>Card number </label>
           <input type="text"></input>
+          <button onClick={onConfirm}>Confirm</button>
+          {paymentConfirm ? <div>Confirm received</div> : <div></div>}
         </div> : <div></div>}
         {paymentMode === 2 ? <div>
           <h4>Crypto</h4>
           <p>Amount : {amount}€</p>
           <div>Send to wallet 1Lbcfr7sAHTD9CgdQo3HTMTkV8LK4ZnX71</div>
-          <button onClick={() => {
-            setTimeout(() => {
-              setPaymentConfirm(true);
-            }, 1000);
-          }}>Confirm</button>
+          <button onClick={onConfirm}>Confirm</button>
           {paymentConfirm ? <div>Confirm received</div> : <div></div>}
         </div> : <div></div>}
       </div>
