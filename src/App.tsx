2a63419b9d2d78a8412d8919889e258c7a5ddcb7
Leonard <leonard@mimisiku.dev>
2024-06-17 12:38:09 +0200

    styling fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 
+enum PaymentMode {
+  Transfer = 0,
+  Card = 1,
+  Crypto = 2
+}
+
 function App() {
   const [amount, setAmount] = useState(0);
   const [paymentMode, setPaymentMode] = useState(0);
@@ -24,42 +30,41 @@ function App() {
       <h1>Lyzi payment</h1>
       <div className="card">
         <p>
-          <span>Amount :</span>
+          <span>Amount: </span>
           <input type="number" onInput={(e) => {
             setInvalidAmount(false);
             setAmount(parseFloat((e.target as HTMLInputElement).value));
           }}></input>€
-          {invalidAmount ? <div>Invalid amount</div> : <div></div>}
+          {invalidAmount ? <div className="invalid">Invalid amount</div> : <div></div>}
         </p>
       </div>
       <div>
         <h3>Payment options :</h3>
         <div>
-          <button onClick={() => setPaymentMode(0)}>Transfer</button>
-          <button onClick={() => setPaymentMode(1)}>Card</button>
-          <button onClick={() => setPaymentMode(2)}>Crypto</button>
+          <button onClick={() => setPaymentMode(PaymentMode.Transfer)}>Transfer</button>
+          <button onClick={() => setPaymentMode(PaymentMode.Card)}>Card</button>
+          <button onClick={() => setPaymentMode(PaymentMode.Crypto)}>Crypto</button>
         </div>
-        {paymentMode === 0 ? <div>
+        {paymentMode === PaymentMode.Transfer ? <div>
           <h4>Transfer</h4>
           <div>Send {amount}€ to FR76 1000 1000 1000 1000</div>
           <button onClick={onConfirm}>Confirm</button>
-          {paymentConfirm ? <div>Confirm received</div> : <div></div>}
         </div> : <div></div>}
-        {paymentMode === 1 ? <div>
+        {paymentMode === PaymentMode.Card ? <div>
           <h4>Card</h4>
           <p>Amount : {amount}€</p>
           <label>Card number </label>
           <input type="text"></input>
           <button onClick={onConfirm}>Confirm</button>
-          {paymentConfirm ? <div>Confirm received</div> : <div></div>}
         </div> : <div></div>}
-        {paymentMode === 2 ? <div>
+        {paymentMode === PaymentMode.Crypto ? <div>
           <h4>Crypto</h4>
           <p>Amount : {amount}€</p>
           <div>Send to wallet 1Lbcfr7sAHTD9CgdQo3HTMTkV8LK4ZnX71</div>
           <button onClick={onConfirm}>Confirm</button>
-          {paymentConfirm ? <div>Confirm received</div> : <div></div>}
         </div> : <div></div>}
+
+        {paymentConfirm ? <div className="valid">Confirm received</div> : <div></div>}
       </div>
     </>
   )
